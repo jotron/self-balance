16749b1de391ba65f4f88b568da745da6cd94998
Jotron <jotron@protonmail.com>
2017-06-15 16:53:25 +0200

    Initial

---
+++ MPU6050_myraw/MPU6050_myraw.ino
@@ -0,0 +1,74 @@
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include "Wire.h"
+
+
+MPU6050 accelgyro;
+//MPU6050 accelgyro(0x69); // <-- use for AD0 high
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+int xangle;
+
+#define LED_PIN 13
+bool blinkState = false;
+
+void setup() {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    // initialize serial communication
+    // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
+    // it's really up to you depending on your project)
+    Serial.begin(9600);
+
+    // initialize device
+    Serial.println("Initializing I2C devices...");
+    accelgyro.initialize();
+
+    // verify connection
+    Serial.println("Testing device connections...");
+    Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+
+
+    //accelgyro.setXGyroOffset(220);
+    //accelgyro.setYGyroOffset(76);
+    //accelgyro.setZGyroOffset(-85);
+    
+
+    // configure Arduino LED for
+    pinMode(LED_PIN, OUTPUT);
+
+    xangle = 0;
+}
+
+void loop() {
+    // read raw accel/gyro measurements from device
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+    // these methods (and a few others) are also available
+    //accelgyro.getAcceleration(&ax, &ay, &az);
+    //accelgyro.getRotation(&gx, &gy, &gz);
+
+    Serial.print("a/g:\t");
+    Serial.print(ax); Serial.print("\t");
+    Serial.print(ay); Serial.print("\t");
+    Serial.print(az); Serial.print("\t");
+    Serial.print(gx); Serial.print("\t");
+    Serial.print(gy); Serial.print("\t");
+    Serial.print(gz); Serial.print("\t");
+
+    xangle = 0.98 * (xangle * 0.978 + gx * 0.012) + 0.02 * (ax);
+    Serial.println(xangle);
+
+    // blink LED to indicate activity
+    blinkState = !blinkState;
+    digitalWrite(LED_PIN, blinkState);
+    delay(10);
+}
---
+++ l298n/l298n.ino
@@ -0,0 +1,63 @@
+// Program: Control 2 DC motors using L298N H Bridge http://qqtrading.com.my/stepper-motor-driver-module-L298N
+
+// Definitions Arduino pins connected to input H Bridge
+int IN1 = 4;
+int IN2 = 5;
+int speedPinA = 9; // Needs to be a PWM pin to be able to control motor speed
+
+int IN3 = 6;
+int IN4 = 7;
+int speedPinB = 10; // Needs to be a PWM pin to be able to control motor speed
+
+  
+void setup()
+{
+ // Set the output pins
+ pinMode(IN1, OUTPUT);
+ pinMode(IN2, OUTPUT);
+ pinMode(IN3, OUTPUT);
+ pinMode(IN4, OUTPUT);
+ pinMode(speedPinA,OUTPUT);
+ pinMode(speedPinB,OUTPUT);
+}
+
+void loop()
+{
+ analogWrite(speedPinA, 255); // Sets speed variable via PWM
+ analogWrite(speedPinB, 255); // Sets speed variable via PWM
+ // Rotate the Motor A clockwise
+ digitalWrite(IN1, HIGH);
+ digitalWrite(IN2, LOW);
+ delay(2000);
+ // Motor A
+ digitalWrite(IN1, HIGH);
+ digitalWrite(IN2, HIGH);
+ delay(500);
+
+ // Rotate the Motor B clockwise
+ digitalWrite(IN3, HIGH);
+ digitalWrite(IN4, LOW);
+ delay(2000);
+ // Motor B
+ digitalWrite(IN3, HIGH);
+ digitalWrite(IN4, HIGH);
+ delay(500);
+
+ // Rotates the Motor A counter-clockwise
+ digitalWrite(IN1, LOW);
+ digitalWrite(IN2, HIGH);
+ delay(2000);
+ // Motor A
+ digitalWrite(IN1, HIGH);
+ digitalWrite(IN2, HIGH);
+ delay(500);
+
+ // Rotates the Motor B counter-clockwise
+ digitalWrite(IN3, LOW);
+ digitalWrite(IN4, HIGH);
+ delay(2000);
+ // Motor B
+ digitalWrite(IN3, HIGH);
+ digitalWrite(IN4, HIGH);
+ delay(500);
+}
